b3c3b030a20b105d0efcbb195f3bfe3aee93afb4
Subhrodeep0019 <161623562+Subhrodeep0019@users.noreply.github.com>
2026-09-13 19:50:07 +0530

    Fix Uvicorn command to point to src:app

--- src/start.sh
+++ src/start.sh
@@ -8,4 +8,4 @@ celery -A src.celery_task:c_app worker --loglevel=info --concurrency=1 &
 
 # Run Uvicorn pointing to the file inside src containing your FastAPI app
 # Replace 'main' with your actual filename if it is not main.py (e.g., src.app:app)
-uvicorn src.main:app --host 0.0.0.0 --port $PORT
+uvicorn src:app --host 0.0.0.0 --port $PORT
