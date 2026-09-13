c41e558a7e393a1f2bf1113a1a02648d0e921221
Subhrodeep <mitrasubhrodeep@gmail.com>
2026-09-13 19:23:39 +0530

    Fix executable start script

--- src/start.sh
+++ src/start.sh
@@ -1,4 +1,4 @@
 
-celery -A celery_task.c_app worker --loglevel=info --concurrency=1 &
+celery -A celery_task.py:c_app worker --loglevel=info --concurrency=1 &
 
 uvicorn src:app --host 0.0.0.0 --port $PORT
