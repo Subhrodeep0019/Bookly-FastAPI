b4f71d97374184f6380572b1cfe5f3a93fd3c918
Subhrodeep <mitrasubhrodeep@gmail.com>
2026-09-13 19:18:13 +0530

    Add executable start script

--- src/start.sh
+++ src/start.sh
@@ -1,4 +1,4 @@
 
-celery -A your_project_name.celery worker --loglevel=info --concurrency=1 &
+celery -A celery_task.c_app worker --loglevel=info --concurrency=1 &
 
-uvicorn your_project_name.main:app --host 0.0.0.0 --port $PORT
+uvicorn src:app --host 0.0.0.0 --port $PORT
